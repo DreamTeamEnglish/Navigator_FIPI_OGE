c6e1d6a9b93f89c3950a97453030d0de527a3029
Dream Team English <13baksmil@gmail.com>
2026-08-29 13:16:42 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,12 @@
 // Supabase service_role keys, passwords, or other secrets in this file.
 
 window.OGE_FIREBASE_CONFIG = Object.freeze({
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyASkYQ2AEDHo0acJx6oKzYFkldgF715Ndw",
+  authDomain: "dte-auth-pilot.firebaseapp.com",
+  projectId: "dte-auth-pilot",
+  storageBucket: "dte-auth-pilot.firebasestorage.app",
+  messagingSenderId: "383382536615",
+  appId: "1:383382536615:web:4b83bd3262f9298abd3682"
 });
 
 // measurementId is NOT required for Firebase Authentication.
